3ea4b681beac3a0793e1416f2f8725ea834b53e8
Akhmat Temukuev <uncreated404@gmail.com>
2025-06-02 09:17:51 +0300

    Update common.go

--- internal/broker/common.go
+++ internal/broker/common.go
@@ -8,7 +8,7 @@ import (
 type Brokers struct {
 	Producer *request.Producer
 }
-
+//for init broker
 func InitBrokers(dialer *kafka.Dialer) (*Brokers, error) {
 	return &Brokers{
 		Producer: request.NewProducer(dialer),
